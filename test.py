1952868d82be7a411ec1afd8bf11bca179a743c6
Chris Zhang <twodan11764@gmail.com>
2019-10-12 12:18:25 -0400

    Update test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-sendemail(from_addr    = 'neth.kuru@gmail.com',
-          to_addr_list = ['nkurukulasuriya@gmail.com'],
-          subject      = 'Howdy',
-          message      = 'Howdy from a python function',
-          login        = 'pythonuser',
-          password     = 'XXXXX')
